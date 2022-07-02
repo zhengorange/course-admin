3f8452ab4706358a69d4bfe22daaa9f3a1b59491
lucheng <super9827@163.com>
2022-07-02 19:26:15 +0800

    。

--- src/views/student/checkCourse/checkCourse.vue
+++ src/views/student/checkCourse/checkCourse.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <CourseTable :course="course" :displayProperty="['courseName']" />
 </template>
 
 <script>
+import CourseTable from "@/components/courseTable.vue";
+import {
+  allSelectCourse
+} from "@/apis/student";
 export default {
-
-}
+  components: {
+    CourseTable
+  },
+  created() {
+    allSelectCourse().then((res) => {
+      this.course = res.data.map(e=>{
+        return {
+            ...e,
+            time: e['courseTime']
+        }
+      })
+    });
+  },
+  data() {
+    return {
+      course: [],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/views/student/checkGrade/checkGrade.vue
+++ src/views/student/checkGrade/checkGrade.vue
@@ -1,13 +1,47 @@
 <template>
-  
+  <div>
+    <el-table :data="tableData" border v-loading="loading">
+      <el-table-column prop="courseId" label="课程编号" align="center">
+      </el-table-column>
+      <el-table-column prop="courseName" label="课程名称" align="center">
+      </el-table-column>
+      <el-table-column prop="grade" label="成绩" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.grade ? scope.row.grade: '暂无成绩' }}
+        </template>
+      </el-table-column>
+      <el-empty slot="empty" description="暂无数据"></el-empty>
+    </el-table>
+  </div>
 </template>
 
 <script>
-export default {
+import { allSelectCourse } from "@/apis/student";
 
-}
+export default {
+  data() {
+    return {
+      tableData: [],
+      loading: false,
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      this.loading = true;
+      allSelectCourse()
+        .then((res) => {
+          this.tableData = res.data;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
 </style>
